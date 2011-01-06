d86846ec92e5f7573091496496a0040823a8981a
david.pinheiro@gorilux.org <david.pinheiro@gorilux.org@71343c49-6f55-db97-15d3-436d42bb6bb9>
2011-01-06 21:44:03 +0000

    Initial commit

---
+++ EFIDuino/Adc.h
@@ -0,0 +1,56 @@
+
+
+#ifndef _ADC_H
+#define _ADC_H
+
+
+#include <WProgram.h>
+
+enum ADCInputType{
+  VBATT, // Battery voltage            - A0
+  TPS,   // throttle position sensor   - A1
+  CLT,   // Colant temperature         - A2
+  MAT,   // Manifold Air temperature   - A3
+  MAP,   // Manifold Absolute Pressure - A4
+  KNOCK, // Knock sensor               - A5
+  EGO,   // Exhaust Gas Oxygen         - A6   
+  MAX_ADC_INPUTS
+};
+
+
+
+class ADCInput{
+private:
+  volatile int value;
+public:
+  uint8_t AnalogPin;
+  uint8_t AnalogReference; 
+  ADCInput(uint8_t analogPin, uint8_t mode);
+  int Value();
+  int SetValue(volatile int value){ 
+    this->value = value; 
+  }
+};
+
+
+
+extern ADCInput VBatt;
+extern ADCInput Tps;
+extern ADCInput Clt;
+extern ADCInput Mat;
+extern ADCInput Map;
+extern ADCInput Knock;
+extern ADCInput Ego;
+
+void StartADCs();
+
+#endif //_ADC_H
+
+
+
+
+
+
+
+
+
---
+++ EFIDuino/EFIDuino.pde
@@ -0,0 +1,44 @@
+
+/**
+ * EFIDuino - An Arduino EMS!
+ * 
+ * 
+ **/
+
+
+#include "Adc.h"
+
+taskLoop(Adc){
+
+  StartADCs();
+
+  delay(100);
+
+
+
+}
+
+void setup(){
+
+
+      
+  Serial.begin(9600);
+
+  createTaskLoop(Adc, NORMAL_PRIORITY);
+  initMainLoopPriority(NORMAL_PRIORITY);
+
+  
+}
+
+
+void loop(){
+
+
+
+  delay(1000);
+
+}
+
+
+
+
